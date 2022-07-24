18c8a7586abe99f382e6ff9d7aec7c361de8c22c
manil-123 <punmanil3@gmail.com>
2022-07-24 13:19:39 +0545

    delete it later

---
+++ lib/view/home/home_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:saral_urja/constants/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var response = Response.fromJson(jsonData);
+  String? provinceValue;
+  String? districtValue;
+  List<String>? provinceList;
+  List<List<Districts>>? districtList;
+  int index = 0;
+
+  getData() {
+    provinceValue = response.data![0].name;
+    provinceList = (response.data!.map((e) => e.name!).toList());
+    districtList = response.data!.map((e) => e.districts!).toList();
+    districtValue = districtList![index].map((e) => e.name!).toList()[0];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Try'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              DropdownButton<String>(
+                value: provinceValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    index = provinceList!.indexOf(newValue!);
+                    print(index);
+                    getData();
+                    provinceValue = newValue!;
+                  });
+                },
+                items:
+                    provinceList!.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              DropdownButton<String>(
+                value: districtValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    //index = provinceList!.indexOf(newValue!);
+                    districtValue = newValue!;
+                  });
+                },
+                items: (districtList![index].map((e) => e.name!).toList())
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ));
+  }
+}
